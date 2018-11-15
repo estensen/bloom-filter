b1167c3b74abc8e55e4be45902482f19cee567e1
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-11-15 11:21:54 +0100

    Create BloomFilter class

--- bloom_filter.py
+++ bloom_filter.py
@@ -1,26 +1,29 @@
 import pyhash
 
-
-# Init storage and hash functions
-bit_vector = [0] * 20
-print(bit_vector)
 fnv_hasher = pyhash.fnv1_32()
 murmur_hasher = pyhash.murmur3_32()
 
 
-def add_item(item):
-    fnv_hash = fnv_hasher(item) % 20
-    print(f'FNV hash of {item}: {fnv_hash}')
-    bit_vector[fnv_hash] = 1
+class BloomFilter:
+    def __init__(self):
+        self.bit_vector = [0] * 20
 
-    murmur_hash = murmur_hasher(item) % 20
-    print(f'Murmur hash of {item}: {murmur_hash}')
-    bit_vector[murmur_hash] = 1
+    def add_item(self, item):
+        fnv_hash = fnv_hasher(item) % 20
+        print(f'FNV hash of {item}: {fnv_hash}')
+        self.bit_vector[fnv_hash] = 1
 
-    print(f'Bit vector after adding {item}: {bit_vector}')
+        murmur_hash = murmur_hasher(item) % 20
+        print(f'Murmur hash of {item}: {murmur_hash}')
+        self.bit_vector[murmur_hash] = 1
+
+        print(f'Bit vector after adding {item}: {self.bit_vector}')
 
 
-# Add cat, dog and goat
-add_item('cat')
-add_item('dog')
-add_item('goat')
+if __name__ == '__main__':
+    bloom_filter = BloomFilter()
+
+    # Add cat, dog and goat
+    bloom_filter.add_item('cat')
+    bloom_filter.add_item('dog')
+    bloom_filter.add_item('goat')
