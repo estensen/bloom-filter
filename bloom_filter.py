056306ee7b9de508c254e5b4a88b62da8a3a5eab
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-11-15 11:31:20 +0100

    Add contains method

--- bloom_filter.py
+++ bloom_filter.py
@@ -8,22 +8,35 @@ class BloomFilter:
     def __init__(self):
         self.bit_vector = [0] * 20
 
-    def add_item(self, item):
-        fnv_hash = fnv_hasher(item) % 20
+    def add(self, item):
+        fnv_hash, murmur_hash = self.hash(item)
         print(f'FNV hash of {item}: {fnv_hash}')
-        self.bit_vector[fnv_hash] = 1
-
-        murmur_hash = murmur_hasher(item) % 20
         print(f'Murmur hash of {item}: {murmur_hash}')
+
+        self.bit_vector[fnv_hash] = 1
         self.bit_vector[murmur_hash] = 1
 
         print(f'Bit vector after adding {item}: {self.bit_vector}')
+
+    def contains(self, item):
+        fnv_hash, murmur_hash = self.hash(item)
+        return self.bit_vector[fnv_hash] and self.bit_vector[murmur_hash]
+
+    @staticmethod
+    def hash(item):
+        fnv_hash = fnv_hasher(item) % 20
+        murmur_hash = murmur_hasher(item) % 20
+
+        return fnv_hash, murmur_hash
 
 
 if __name__ == '__main__':
     bloom_filter = BloomFilter()
 
     # Add cat, dog and goat
-    bloom_filter.add_item('cat')
-    bloom_filter.add_item('dog')
-    bloom_filter.add_item('goat')
+    bloom_filter.add('cat')
+    bloom_filter.add('dog')
+    bloom_filter.add('goat')
+
+    print(f"Contains 'cat'? {bloom_filter.contains('cat')}")
+    print(f"Contains 'lion'? {bloom_filter.contains('lion')}")
